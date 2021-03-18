6fdafa6692053e74d61ff4c1f551d9159c161a15
Jason Flashner <jasonflashner@Jasons-MacBook-Pro.local>
2021-03-18 10:56:24 -0400

    implemented bootstrap

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
 import GifTile from "../tiles/GifTile"
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
+
 
 const HomeContainer = (props) => {
     const [search, setSearch] = useState("")
@@ -25,30 +30,53 @@ const HomeContainer = (props) => {
         setSearch(value.currentTarget.value)
     }
 
-    let gifTiles = []
-    if (gifs.length !== 0) {
-        for (let i=0; i<3; i++) {
-            gifTiles.push(<GifTile
-                key = {gifs[featuredTile + i]["id"]}
-                url = {gifs[featuredTile + i]["images"]["downsized_large"]["url"]}
-                />)
-        }
-    }
-
     const add = (value) => {
         value.preventDefault()
-        setFeaturedTile(featuredTile + 1)
+        if (value.currentTarget.id === "+") {
+            setFeaturedTile(featuredTile + 1)
+        } else if (value.currentTarget.id === "-")
+            if (featuredTile !== 0 ) {
+                setFeaturedTile(featuredTile - 1)
+            }
+    }
+
+    let gifTiles = []
+    if (gifs.length !== 0) {
+        gifTiles.push(
+        <Col>
+            <button onClick = {add} id="-">&lt;-</button>
+        </Col>
+        )
+        for (let i=0; i<3; i++) {
+            gifTiles.push(
+            <Col>
+                <GifTile
+                    key = {gifs[featuredTile + i]["id"]}
+                    url = {gifs[featuredTile + i]["images"]["downsized"]["url"]}
+                />
+            </Col>)
+        }
+        gifTiles.push(
+            <Col>
+                <button onClick = {add} id="+">-&gt;</button>
+            </Col>
+        )
     }
 
     return (
         <div>
-            <form>
-                <label>Search: </label>
-                <input type="text" id="search" name="search" onChange = {handleChange} ></input>
-                <button onClick = {onSubmit}> Submit</button>
-                {gifTiles}
-                <button onClick = {add}>+</button>
-            </form>
+            <Container fluid>
+                <Row>
+                    <form>
+                        <label>Search: </label>
+                        <input type="text" id="search" name="search" onChange = {handleChange} ></input>
+                        <Button onClick = {onSubmit}> Submit</Button>
+                    </form>
+                </Row>
+                <Row>
+                    {gifTiles}
+                </Row>
+            </Container>
         </div>
     );
 };
--- src/tiles/GifTile.js
+++ src/tiles/GifTile.js
@@ -2,11 +2,9 @@ import React from "react";
 
 const GifTile = (props) => {
     const url = props.url
-    const count = props.count
 
     return (
         <div>
-            <p>{count}</p>
             <img src={url} alt={url}/>
         </div>
     );
